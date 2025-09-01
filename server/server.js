525f537f17d83466105fa51c2ee998c3cb604f81
Aman Murya <m.amanmaurya49@gmail.com>
2025-09-01 08:42:52 +0530

    cors policy

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import './config/db.js';
 import morgan from 'morgan';
 import dotenv from 'dotenv'
+import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,26 @@ dotenv.config();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors)
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://care-bridge-three.vercel.app"
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
