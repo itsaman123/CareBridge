e494dd147fe63c132e8af5a36d92bda78260e85f
Aman Murya <m.amanmaurya49@gmail.com>
2025-11-23 11:55:58 +0530

    disable buffer for mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI
+
+
+mongoose.set('bufferCommands', false);
+mongoose.set('bufferTimeoutMS', 0);
+
 mongoose.connect(MONGODB_URI)
     .then(() => console.log('mongoDB connected'))
     .catch((err) => console.log('MongoDB Connection Error ', err))
