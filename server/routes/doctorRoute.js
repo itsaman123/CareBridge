d22af0aa34816f84be36c2175a986a4fdf572f77
Aman Murya <m.amanmaurya49@gmail.com>
2026-02-05 10:02:37 +0530

    backend

--- server/routes/doctorRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginDoctor, doctorProfile, updateDoctorProfile } from '../controllers/doctorsController.js';
-const doctorRouter = express.Router();
-
-doctorRouter.post('/login', loginDoctor)
-doctorRouter.get('/profile', doctorProfile)
-doctorRouter.post('/update-profile', updateDoctorProfile)
-
-export default doctorRouter
